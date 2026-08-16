36da291fb392cdd0c564fa1a5dee18fe897e40f2
sonam khandait <sonamkhandait44@gmail.com>
2026-08-16 15:52:47 +0530

    Update S3 bucket name in backend configuration

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "devsecops-2048-sona-kh-4pm" # Replace with your actual S3 bucket name
+    bucket = "devsecops-2048-sona-khandait-4pm" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
